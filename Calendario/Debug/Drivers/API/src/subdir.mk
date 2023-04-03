57be15e60f6d1c2e6e7829a088486100540069f0
isipavelek <ipavelek@gmail.com>
2023-04-02 23:28:30 -0300

    puntero HAL I2C

---
+++ Calendario/Debug/Drivers/API/src/subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/API/src/API_Init.c \
+../Drivers/API/src/API_debounce.c \
+../Drivers/API/src/API_delay.c \
+../Drivers/API/src/API_enc_port.c \
+../Drivers/API/src/API_encoder.c \
+../Drivers/API/src/API_lcd.c \
+../Drivers/API/src/API_lcd_port.c \
+../Drivers/API/src/API_reloj.c \
+../Drivers/API/src/API_rtc.c \
+../Drivers/API/src/API_uart.c 
+
+OBJS += \
+./Drivers/API/src/API_Init.o \
+./Drivers/API/src/API_debounce.o \
+./Drivers/API/src/API_delay.o \
+./Drivers/API/src/API_enc_port.o \
+./Drivers/API/src/API_encoder.o \
+./Drivers/API/src/API_lcd.o \
+./Drivers/API/src/API_lcd_port.o \
+./Drivers/API/src/API_reloj.o \
+./Drivers/API/src/API_rtc.o \
+./Drivers/API/src/API_uart.o 
+
+C_DEPS += \
+./Drivers/API/src/API_Init.d \
+./Drivers/API/src/API_debounce.d \
+./Drivers/API/src/API_delay.d \
+./Drivers/API/src/API_enc_port.d \
+./Drivers/API/src/API_encoder.d \
+./Drivers/API/src/API_lcd.d \
+./Drivers/API/src/API_lcd_port.d \
+./Drivers/API/src/API_reloj.d \
+./Drivers/API/src/API_rtc.d \
+./Drivers/API/src/API_uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/API/src/%.o Drivers/API/src/%.su Drivers/API/src/%.cyclo: ../Drivers/API/src/%.c Drivers/API/src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F429ZITx -DSTM32F4 -DNUCLEO_F429ZI -DUSE_HAL_DRIVER -DSTM32F429xx -c -I../Inc -I"C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc" -I"C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Include" -I"C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/BSP/STM32F4xx_Nucleo_144" -I"C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/Core/Inc" -I"C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/API/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-API-2f-src
+
+clean-Drivers-2f-API-2f-src:
+	-$(RM) ./Drivers/API/src/API_Init.cyclo ./Drivers/API/src/API_Init.d ./Drivers/API/src/API_Init.o ./Drivers/API/src/API_Init.su ./Drivers/API/src/API_debounce.cyclo ./Drivers/API/src/API_debounce.d ./Drivers/API/src/API_debounce.o ./Drivers/API/src/API_debounce.su ./Drivers/API/src/API_delay.cyclo ./Drivers/API/src/API_delay.d ./Drivers/API/src/API_delay.o ./Drivers/API/src/API_delay.su ./Drivers/API/src/API_enc_port.cyclo ./Drivers/API/src/API_enc_port.d ./Drivers/API/src/API_enc_port.o ./Drivers/API/src/API_enc_port.su ./Drivers/API/src/API_encoder.cyclo ./Drivers/API/src/API_encoder.d ./Drivers/API/src/API_encoder.o ./Drivers/API/src/API_encoder.su ./Drivers/API/src/API_lcd.cyclo ./Drivers/API/src/API_lcd.d ./Drivers/API/src/API_lcd.o ./Drivers/API/src/API_lcd.su ./Drivers/API/src/API_lcd_port.cyclo ./Drivers/API/src/API_lcd_port.d ./Drivers/API/src/API_lcd_port.o ./Drivers/API/src/API_lcd_port.su ./Drivers/API/src/API_reloj.cyclo ./Drivers/API/src/API_reloj.d ./Drivers/API/src/API_reloj.o ./Drivers/API/src/API_reloj.su ./Drivers/API/src/API_rtc.cyclo ./Drivers/API/src/API_rtc.d ./Drivers/API/src/API_rtc.o ./Drivers/API/src/API_rtc.su ./Drivers/API/src/API_uart.cyclo ./Drivers/API/src/API_uart.d ./Drivers/API/src/API_uart.o ./Drivers/API/src/API_uart.su
+
+.PHONY: clean-Drivers-2f-API-2f-src
+
---
+++ Calendario/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dcmi.d
@@ -0,0 +1,74 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dcmi.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dcmi.c \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Inc/stm32f4xx_hal_conf.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma2d.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dcmi.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_eth.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sdram.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_fmc.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_ltdc.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_ltdc_ex.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dsi.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Inc/stm32f4xx_hal_conf.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma2d.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dcmi.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_eth.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_sdram.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_fmc.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_ltdc.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_ltdc_ex.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dsi.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+C:/Users/ipave/Documents/GitHub/Protocolos_workspace/Calendario/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
