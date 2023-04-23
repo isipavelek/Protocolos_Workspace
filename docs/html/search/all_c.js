79685d4b632718b12dd84060a9ce96cd83e85d89
isipavelek <ipavelek@gmail.com>
2023-04-23 16:42:46 -0300

    doc

---
+++ docs/html/search/all_c.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['marzo_0',['MARZO',['../d7/dbc/API__reloj_8h.html#aa5dcc4a78d7f307746b30383f943bdefa3a9e73743853fa61769ba4d7c642665f',1,'API_reloj.h']]],
+  ['mayo_1',['MAYO',['../d7/dbc/API__reloj_8h.html#aa5dcc4a78d7f307746b30383f943bdefa994c336f9f6a8b45059b4565e36073a0',1,'API_reloj.h']]],
+  ['mes_2',['mes',['../d4/db4/structreloj.html#a6b03af66cbece976ed09e25d7572bad5',1,'reloj']]],
+  ['mes31_3',['mes31',['../d2/d41/API__reloj_8c.html#a18eae73cfa0378350b1c737212167203',1,'API_reloj.c']]],
+  ['meses_5fbcd_4',['meses_BCD',['../d7/dbc/API__reloj_8h.html#aa5dcc4a78d7f307746b30383f943bdef',1,'API_reloj.h']]],
+  ['mesmax_5',['MESMAX',['../d7/dbc/API__reloj_8h.html#aca42a159262afcc6cc90a00a216ccd08',1,'API_reloj.h']]],
+  ['mesmin_6',['MESMIN',['../d7/dbc/API__reloj_8h.html#a024bd933774a5eef236554b9409b52fc',1,'API_reloj.h']]],
+  ['min_7',['min',['../d4/db4/structreloj.html#ac9b481208b43f7c37ed25e446bdec692',1,'reloj']]],
+  ['minmax_8',['MINMAX',['../d7/dbc/API__reloj_8h.html#ab0052b2fbcf86b26cf6bc789c3566dea',1,'API_reloj.h']]],
+  ['minmin_9',['MINMIN',['../d7/dbc/API__reloj_8h.html#a715f8aafaa8af745874caf905d66dd68',1,'API_reloj.h']]]
+];
---
+++ docs/html/search/variables_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['running_0',['running',['../d4/d1d/structdelay__t.html#a873eb02d943c873d26b7ea9c74a0fb59',1,'delay_t']]]
+];
